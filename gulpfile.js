86b369124439257f402197c1967c24e4c01ba7a1
larsingerlars <larsingerlars@gmail.com>
2022-01-20 18:27:29 +0200

    FEAT: refactoring js + scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const autoprefixer = require("gulp-autoprefixer");
-const rename = require("gulp-rename");
-const cleanCSS = require("gulp-clean-css");
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
 
 function css() {
-  return gulp
-    .src("./scss/main.scss")
-    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("./css/"));
+    return gulp
+        .src('./scss/main.scss')
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css/'))
 }
 
 function watchFiles() {
-  gulp.watch("./scss/**/*.scss", css);
+    gulp.watch('./scss/**/*.scss', css)
 }
 
 function minifyCSS() {
-  return gulp
-    .src("./css/main.css")
-    .pipe(cleanCSS())
-    .pipe(
-      rename({
-        suffix: ".min",
-      })
-    )
-    .pipe(gulp.dest("./css/"));
+    return gulp
+        .src('./css/main.css')
+        .pipe(cleanCSS())
+        .pipe(
+            rename({
+                suffix: '.min'
+            })
+        )
+        .pipe(gulp.dest('./css/'))
 }
 
 function buildFiles() {
-  gulp.watch("./src/scss/**/*.scss", css);
-  gulp.watch("./src/css/main.css", minifyCSS);
+    gulp.watch('./scss/**/*.scss', css)
+    gulp.watch('./css/main.css', minifyCSS)
 }
 
 // TASKS
-gulp.task("css", css);
-gulp.task("watch", gulp.parallel(watchFiles));
-gulp.task("minify", minifyCSS);
-gulp.task("build", gulp.parallel(buildFiles));
+gulp.task('css', css)
+gulp.task('watch', gulp.parallel(watchFiles))
+gulp.task('minify', minifyCSS)
+gulp.task('build', gulp.parallel(buildFiles))
--- scripts/script.js
+++ scripts/script.js
@@ -1,107 +1,207 @@
+'use strict'
+
 // GLOBAL VARIABLES
-const bg = document.querySelector(".bg");
-const heading = document.querySelector(".timer__heading");
-const timer = document.querySelectorAll(".timer__display > *");
-const welcome = document.querySelector(".timer__welcome");
-const birthday = document.querySelector(".timer__birthday");
-const msg = document.querySelector(".msg");
-const link = document.querySelector(".link");
-const values = Array.from(timer);
-const valueText = document.querySelectorAll(".value");
-const github = document.querySelector(".github");
-const footer = document.querySelector("footer");
+const bg = document.querySelector('.bg')
+const heading = document.querySelector('.timer__heading')
+const timer = document.querySelectorAll('.timer__display > *')
+const msg = document.querySelector('.msg')
+const link = document.querySelector('.link')
+const values = Array.from(timer)
+const valueText = document.querySelectorAll('.value')
+const github = document.querySelector('.github')
+const footer = document.querySelector('footer')
+
+const BIRTHDATE = new Date(2021, 1, 12)
 
 // COUNTDOWN VARIABLES
-const sec = 1000;
-const min = sec * 60;
-const hr = min * 60;
-const day = hr * 24;
+const sec = 1000
+const min = sec * 60
+const hr = min * 60
+const day = hr * 24
 
 // MODE OBJECTS
-const img = { morning: "morning", day: "day", night: "night" };
-const color = { morning: "#e07a51", day: "#44CCEE", night: "#0A1239" };
+const img = { morning: 'morning', day: 'day', night: 'night' }
+const color = { morning: '#e07a51', day: '#44CCEE', night: '#0A1239' }
 
 // VARIABLES DAY-MODE
-const sun = document.querySelector(".sun_day");
-const birdCage = document.querySelector(".birdcage");
+const sun = document.querySelector('.sun_day')
+const birdCage = document.querySelector('.birdcage')
 
 // VARIABLES NIGHT-MODE
-const moon = document.querySelector(".moonlight");
-const stars = document.querySelector(".overlay-stars");
-const owl = document.querySelector(".owl");
+const moon = document.querySelector('.moonlight')
+const stars = document.querySelector('.overlay-stars')
+const owl = document.querySelector('.owl')
 
 // VARIABLES MORNING-MODE
-const sunrise = document.querySelector(".sunlight");
+const sunrise = document.querySelector('.sunlight')
 
 // MODE ARRAYS
-const dayMode = [sun, birdCage];
-const morningMode = [sunrise, owl];
-const nightMode = [moon, stars, owl];
+const dayMode = [sun, birdCage]
+const morningMode = [sunrise, owl]
+const nightMode = [moon, stars, owl]
 
 // LANGUAGE CODES
-const GERMANY_LANGUAGE_CODE = "de-DE";
-const LATVIA_LANGUAGE_CODE = "lv-LV";
+const GERMANY_LANGUAGE_CODE = 'de-DE'
+const LATVIA_LANGUAGE_CODE = 'lv-LV'
 
 // LANGUAGE DETECTION
 const detectLanguage = () => {
-  const language = navigator.language;
-  if (language === GERMANY_LANGUAGE_CODE) {
-    heading.textContent = "Es ist was es ist, sagt die Liebe";
-    welcome.textContent = "Willkommen, kleines Wesen!";
-    msg.textContent = "Mit Liebe gemacht";
-  } else if (language === LATVIA_LANGUAGE_CODE) {
-    heading.textContent = "Ir kā ir. Teica mīlestība.";
-    welcome.textContent = "Laipni lūgts, mazais!";
-    msg.textContent = "Ar mīlestību taisīts";
-  } else {
-    return;
-  }
-};
+    const language = navigator.language
+    if (language === GERMANY_LANGUAGE_CODE) {
+        heading.textContent = 'Es ist was es ist, sagt die Liebe'
+        msg.textContent = 'Mit Liebe gemacht'
+    } else if (language === LATVIA_LANGUAGE_CODE) {
+        heading.textContent = 'Ir kā ir. Teica mīlestība.'
+        msg.textContent = 'Ar mīlestību taisīts'
+    } else {
+        return
+    }
+}
 
 // TIME DETECTION
 const detectTime = () => {
-  const currentTime = new Date().getHours();
-  changeColor(currentTime);
-};
+    const currentTime = new Date().getHours()
+    changeColor(currentTime)
+}
 
 const showComponents = (componentList) => {
-  componentList.forEach((element) => {
-    element.style.display = "block";
-  });
-};
+    componentList.forEach((element) => {
+        element.style.display = 'block'
+    })
+}
 
 const changeColor = (time) => {
-  try {
-    const isDay = time >= 8 && time < 19;
-    const isMorning = time > 5 && time < 8;
-    const isNight = time >= 19 || time < 5;
+    try {
+        const isDay = time >= 8 && time < 19
+        const isMorning = time > 5 && time < 8
+        const isNight = time >= 19 || time < 5
 
-    if (isDay) {
-      bg.style.backgroundImage = `url("./resources/img/bg_${img.day}.png")`;
-      heading.style.backgroundImage = `url("./resources/img/bg_${img.day}.png")`;
-      showComponents(dayMode);
-      welcome.style.color = `${color.day}`;
-      birthday.style.color = `${color.day}`;
-    } else if (isMorning) {
-      bg.style.backgroundImage = `url("./resources/img/bg_${img.morning}.png")`;
-      heading.style.backgroundImage = `url("./resources/img/bg_${img.morning}.png")`;
-      showComponents(morningMode);
-      welcome.style.color = `${color.morning}`;
-      birthday.style.color = `${color.morning}`;
-    } else if (isNight) {
-      bg.style.backgroundImage = `url("./resources/img/bg_${img.night}.png")`;
-      heading.style.backgroundImage = `url("./resources/img/bg_${img.night}.png")`;
-      showComponents(nightMode);
-      msg.style.color = "rgb(250,250,250)";
-      github.style.color = "rgb(250,250,250)";
-      welcome.style.color = `${color.night}`;
-      birthday.style.color = `${color.night}`;
+        if (isDay) {
+            bg.style.backgroundImage = `url("./resources/img/bg_${img.day}.png")`
+            heading.style.backgroundImage = `url("./resources/img/bg_${img.day}.png")`
+            showComponents(dayMode)
+        } else if (isMorning) {
+            bg.style.backgroundImage = `url("./resources/img/bg_${img.morning}.png")`
+            heading.style.backgroundImage = `url("./resources/img/bg_${img.morning}.png")`
+            showComponents(morningMode)
+        } else if (isNight) {
+            bg.style.backgroundImage = `url("./resources/img/bg_${img.night}.png")`
+            heading.style.backgroundImage = `url("./resources/img/bg_${img.night}.png")`
+            showComponents(nightMode)
+            msg.style.color = 'rgb(250,250,250)'
+            github.style.color = 'rgb(250,250,250)'
+        }
+    } catch (error) {
+        console.log(error)
     }
-  } catch (error) {
-    console.log(error);
-  }
-};
+}
+
+// COUNTDOWN
+const updateCountdown = (timeDifference) => {
+    const days = Math.floor(timeDifference / day)
+    const hours = Math.floor((timeDifference % day) / hr)
+    const minutes = Math.floor((timeDifference % hr) / min)
+    const seconds = Math.floor((timeDifference % min) / sec)
+
+    if (days > 9) {
+        values[0].firstElementChild.textContent = `${days}`
+    } else {
+        values[0].firstElementChild.textContent = `0${days}`
+    }
+    if (hours > 9) {
+        values[1].firstElementChild.textContent = `${hours}`
+    } else {
+        values[1].firstElementChild.textContent = `0${hours}`
+    }
+    if (minutes > 9) {
+        values[2].firstElementChild.textContent = `${minutes}`
+    } else {
+        values[2].firstElementChild.textContent = `0${minutes}`
+    }
+    if (seconds > 9) {
+        values[3].firstElementChild.textContent = `${seconds}`
+    } else {
+        values[3].firstElementChild.textContent = `0${seconds}`
+    }
+}
+
+const runCountdown = () => {
+    countdownActive = setInterval(() => {
+        const now = Date.now()
+        const birthday = BIRTHDATE.getTime()
+        const difference = now - birthday
+        updateCountdown(difference)
+    }, sec)
+}
 
 // EVENT LISTENER
-document.addEventListener("load", detectLanguage());
-document.addEventListener("load", detectTime());
+document.addEventListener('load', detectLanguage())
+document.addEventListener('load', detectTime())
+window.addEventListener('load', runCountdown())
+
+// const countdownTimer = () => {
+//   const config = {
+//     timeValues: ["days", "hours", "minutes", "seconds"],
+//     ratio: {
+//       days: 86_400_000,
+//       hours: 3_600_000,
+//       minutes: 60_000,
+//       seconds: 1_000,
+//     },
+//     remainder: {
+//       days: 365,
+//       hours: 24,
+//       minutes: 60,
+//       seconds: 60,
+//     },
+//     rootClass: document.querySelector(".countdown"),
+//   };
+
+//   const elements = getTimeValueElements(config.timeValues, config.rootClass);
+//   setInterval(
+//     () => updateCountdown(elements, config.ratio, config.remainder),
+//     1000
+//   );
+// };
+
+// const getTimeValueElements = (timeValues, root) => {
+//   const elements = [];
+//   // Return all countdown display list elements in an array
+//   timeValues.forEach((value) => elements.push(root.querySelector(`#${value}`)));
+//   return elements;
+// };
+
+// const getCurrentTimeDifference = () => {
+//   const now = Date.now();
+//   // Transform endDate into miliseconds by using getTime()
+//   const endDate = new Date("December 31, 2021 23:00:00").getTime();
+//   return endDate - now;
+// };
+
+// const updateCountdown = (elements, divisors, remainder) => {
+//   const currentTimeDifference = getCurrentTimeDifference();
+//   const keys = Object.keys(divisors);
+
+//   elements.forEach((element) => {
+//     if (keys.includes(element.id)) {
+//       element.textContent = formatOneDigitDisplay(
+//         calculateTimeValue(currentTimeDifference, divisors, remainder, element)
+//       );
+//     }
+//   });
+// };
+
+// const calculateTimeValue = (currentTime, divisor, remainder, element) => {
+//   return Math.floor(
+//     (currentTime / divisor[element.id]) % remainder[element.id]
+//   );
+// };
+
+// const formatOneDigitDisplay = (value) => {
+//   if (value < 10) {
+//     return (value = `0${value}`);
+//   }
+//   return value;
+// };
+
+// countdownTimer();
